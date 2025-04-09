b5500e048826f86ed89347b9ea2ec3b433e949d5
Jonas Limpert <33454605+JonaLimp@users.noreply.github.com>
2025-04-09 19:38:40 +0200

    Fix data fetching

--- scripts/historical_data_fetcher.py
+++ scripts/historical_data_fetcher.py
@@ -4,28 +4,28 @@ from pathlib import Path
 
 import requests
 
-dir_path = Path("raw_data")
-if not dir_path.exists():
-    dir_path.mkdir()
+config = json.load(open("sensor_config.json"))
+FETCH_INTERVAL = config["preprocessing"]["fetch_interval"]
+START_DATE = config["preprocessing"]["start_date"]
 
-with open("sensors.json") as f:
+with open("sensor_config.json") as f:
     sensor_config = json.load(f)
 
 config_sensor_types = sensor_config["sensor_types"]
 sensor_boxes = sensor_config["boxes"]
 
-start_date = datetime.utcnow().date() - timedelta(days=1)
+if START_DATE == "now":
+    start_date = datetime.utcnow().date()
+else:
+    start_date = datetime.strptime(START_DATE, "%Y-%m-%d").date()
+
+start_date = start_date - timedelta(days=1)
 start_datetime = datetime.combine(start_date, datetime.min.time())
-end_date = start_date - timedelta(days=30)
+end_date = start_date - timedelta(days=FETCH_INTERVAL)
 chunk_days = 1
 
-
 for box_name, box_content in sensor_boxes.items():
-    box_path = Path(dir_path) / box_name
-    if not box_path.exists():
-        box_path.mkdir()
-
-    print(box_name)
+    print(f"Processing box: {box_name}")
     box_id = box_content["box_id"]
     url = f"https://api.opensensemap.org/boxes/{box_id}"
     response = requests.get(url)
@@ -37,36 +37,56 @@ for box_name, box_content in sensor_boxes.items():
     box_data = response.json()
     sensors = box_data.get("sensors", [])
 
-    current_end = start_date
-
     for sensor in sensors:
+        current_end = start_date
         sensor_id = sensor["_id"]
 
         if sensor["title"] not in config_sensor_types:
             continue
-        print(sensor["title"])
+
+        print(f"Processing sensor: {sensor['title']}")
+
         while current_end > end_date:
             current_start = max(current_end - timedelta(days=chunk_days - 1), end_date)
-
-            from_str = current_start.strftime("%Y-%m-%dT23:59:59Z")
+            from_str = current_start.strftime("%Y-%m-%dT00:00:00Z")
             to_str = current_end.strftime("%Y-%m-%dT23:59:59Z")
 
             csv_url = (
                 f"https://api.opensensemap.org/boxes/{box_id}/data/{sensor_id}"
-                f"?from={from_str}&to={to_str}&format=csv"
+                f"?from-date={from_str}&to-date={to_str}&format=csv"
             )
 
             csv_response = requests.get(csv_url)
 
-            if response.status_code != 200:
-                print("error!")
+            if csv_response.status_code != 200:
+                print(
+                    f"Error fetching data for {sensor['title']}"
+                    f" from {from_str} to {to_str}"
+                )
+                continue
 
             csv_data = csv_response.text
 
+            if not csv_data.strip():
+                print(f"No data for {sensor['title']} between {from_str} and {to_str}.")
+                continue
+
             file_path = (
-                box_path
-                / f"{box_name.replace(' ', '_')}_{sensor['title']}_{current_start}.csv"
+                Path("data/raw_data")
+                / box_name.replace(" ", "_")
+                / Path(sensor["title"].replace(" ", "_"))
+                / f"{box_name.replace(' ', '_')}_"
+                f"{sensor['title'].replace(' ', '_')}_{current_start}.csv"
             )
-            with file_path.open("w") as f:
+
+            if not file_path.parent.exists():
+                file_path.parent.mkdir(parents=True, exist_ok=True)
+
+            print(
+                f"Writing data for {sensor['title']}"
+                f" between {from_str} and {to_str} to {file_path}"
+            )
+            with file_path.open("w", encoding="utf-8") as f:
                 f.write(csv_data)
+
             current_end = current_start - timedelta(days=1)
